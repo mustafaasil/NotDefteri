e93f13138c287d5706b186dd9d547813903d58aa
Mustafa <mustafa.asill@hotmail.com>
2023-02-02 15:01:25 +0300

    addcors

--- NotDefteriApi/Program.cs
+++ NotDefteriApi/Program.cs
@@ -4,6 +4,7 @@ using NotDefteriApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors();
 var Baglanti = builder.Configuration.GetConnectionString("BaglantiCumlem");
 builder.Services.AddDbContext<UygulamaDbContext>(o => o.UseSqlServer(Baglanti));
 
@@ -22,7 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(p => p.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod()); // herhangi bir adresten gelen istege izin verir, post,del,get istegi gönderebilir
 app.UseAuthorization();
 
 app.MapControllers();
